c341f1cb1b6057b6b2ed8109bc86dba3f01461c9
NievinEvgeny <zhenya-nievin@mail.ru>
2023-04-07 00:19:59 +0700

    Implementation of client's connection to server

---
+++ include/client/connection.h
@@ -0,0 +1,5 @@
+#pragma once
+
+int connect_to_server(char* hostname, int port);
+
+void debug_func(int sockfd);
--- src/client/client.c
+++ src/client/client.c
@@ -1,7 +1,21 @@
+#include <client/connection.h>
+#include <client/error_handler.h>
 #include <stdio.h>
-#include <client/client.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char* argv[])
 {
-    printf("client");
+    if (argc < 3)
+    {
+        fprintf(stderr, "Error arguments\nExample: ./%s ip port\n", argv[0]);
+        exit(0);
+    }
+
+    int sockfd = connect_to_server(argv[1], atoi(argv[2]));
+
+    debug_func(sockfd);
+
+    close(sockfd);
+    return 0;
 }

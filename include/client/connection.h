14ba3ea929a5558229848c13a296a446482ad827
NievinEvgeny <zhenya-nievin@mail.ru>
2023-04-10 07:34:01 +0700

    Delete declarations of debug functions

--- include/client/connection.h
+++ include/client/connection.h
@@ -1,5 +1,3 @@
 #pragma once
 
 int connect_to_server(char* hostname, int port);
-
-void debug_func(int sockfd);
--- include/server/connection.h
+++ include/server/connection.h
@@ -11,5 +11,3 @@ extern pthread_mutex_t mutexcount;
 int setup_listener(int port);
 
 void get_clients(int lis_sockfd, int* cli_sockfd);
-
-void* debug_func(void* thread_data);
